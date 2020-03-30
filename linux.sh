9e03e25a3a611e47ca5a6bf3357765a3a30f865f
James Swineson <github@public.swineson.me>
2020-03-30 06:46:29 -0700

    support alpine

--- linux.sh
+++ linux.sh
@@ -15,21 +15,36 @@ function package_manager_update() {
 	fi
 
 	if command -v pacman; then
+		echo "Updating via pacman..."
+
 		pacman --sync --refresh --sysupgrade --noconfirm
+	fi
+
+	if command -v apk; then
+		echo "Updating via apk..."
+
+		apk update
+		apk upgrade
 	fi
 
 	# note: some versions of OpenSUSE symlinks yum to zypper
 	if command -v yum; then
+		echo "Updating via yum..."
+
 		yum -y update
 	fi
 
 	if command -v zypper; then
+		echo "Updating via zypper..."
+
 		zypper --non-interactive refresh
 		zypper --non-interactive update
 	fi
 
 	# how to use emerge unattended?
 	#if command -v emerge; then
+	#	echo "Updating via emerge..."
+	#
 	#	emerge --sync
 	#	emerge --update --deep --with-bdeps=y @world
 	#fi
