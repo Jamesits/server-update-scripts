1291b85421231ebb7fab58323430172acd88c243
James Swineson <github@public.swineson.me>
2020-04-23 02:42:51 -0700

    do not allow package manager commands to be interrupted

--- linux.sh
+++ linux.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 set -Eeuo pipefail
+TEMP_OUTPUT="/tmp/linux_update.log"
+
+# if we use package manager to update go-agent itself, it will just disconnect so we work around it
+function run() {
+	nohup "$@" > "$TEMP_OUTPUT" 2>&1 &
+	PID=$!
+	tail -f "$TEMP_OUTPUT" &
+	TAIL_PID=$!
+	wait "$PID"
+	kill "$TAIL_PID"
+	rm -f "$TEMP_OUTPUT"
+}
 
 function package_manager_update() {
 	if command -v apt-get; then
@@ -9,44 +21,44 @@ function package_manager_update() {
 		export DEBIAN_FRONTEND=noninteractive
 		export DEBIAN_PRIORITY=critical
 	
-		apt-get update -y
-		apt-get full-upgrade -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold"
-		apt-get autoremove -y
+		run apt-get update -y
+		run apt-get full-upgrade -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold"
+		run apt-get autoremove -y
 	fi
 
 	if command -v pacman; then
 		echo "Updating via pacman..."
 
-		pacman --sync --refresh --sysupgrade --noconfirm
+		run pacman --sync --refresh --sysupgrade --noconfirm
 	fi
 
 	if command -v apk; then
 		echo "Updating via apk..."
 
-		apk update
-		apk upgrade
+		run apk update
+		run apk upgrade
 	fi
 
 	# note: some versions of OpenSUSE symlinks yum to zypper
 	if command -v yum; then
 		echo "Updating via yum..."
 
-		yum -y update
+		run yum -y update
 	fi
 
 	if command -v zypper; then
 		echo "Updating via zypper..."
 
-		zypper --non-interactive refresh
-		zypper --non-interactive update
+		run zypper --non-interactive refresh
+		run zypper --non-interactive update
 	fi
 
 	# how to use emerge unattended?
 	#if command -v emerge; then
 	#	echo "Updating via emerge..."
 	#
-	#	emerge --sync
-	#	emerge --update --deep --with-bdeps=y @world
+	#	run emerge --sync
+	#	run emerge --update --deep --with-bdeps=y @world
 	#fi
 }
 
